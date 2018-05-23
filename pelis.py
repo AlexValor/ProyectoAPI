6fc0c60306cc14c427b63da23dcc0712cb9f31e8
Alejandro Valor <36158323+AlexValor@users.noreply.github.com>
2018-05-23 17:17:36 +0200

    Actualizado v2

--- pelis.py
+++ pelis.py
@@ -1,140 +1,167 @@
 import requests
 import json
+from colorama import Back, Style, Fore, init
+init(autoreset=True)
 
 raiz= "http://www.omdbapi.com/?apikey=2cbd7158&"
 
-print('Tipos de busqueda: ')
+print(Fore.GREEN + Style.BRIGHT + 'Tipos de busqueda: ')
 print()
-print('1. Peliculas')
-print('2. Series')
-print('3. Otro tipo')
-print('4. Salir')
+print(Fore.BLUE + Style.BRIGHT + '1. Peliculas')
+print(Fore.BLUE + Style.BRIGHT + '2. Series')
+print(Fore.BLUE + Style.BRIGHT + '3. Otro tipo')
+print(Fore.BLUE + Style.BRIGHT + '4. Salir')
 print()
 
-eleccion=int(input("Escoge una opción: "))
+eleccion=int(input(Fore.GREEN + Style.BRIGHT+ "Escoge una opción: "))
 eleccion2=""
 
 if eleccion == 1:
 #Para hacer un request de peliculas solamente.
-	buscar= input("Dime una película: ")
+	buscar= input(Fore.GREEN+ Style.BRIGHT +"Dime una película: ")
 	peliculas=requests.get(raiz,
 									params={'s':buscar,
 									'type':'movie'}).json()
 
-	for x in range (10):
-		print(peliculas['Search'][x]['Title'])
-		print("ID: ",peliculas['Search'][x]['imdbID'])
+	for x in range (5):
+		print(Fore.YELLOW + Style.BRIGHT +"Busquedas encontradas: ")
+		print(Fore.BLUE + Style.BRIGHT + peliculas['Search'][x]['Title'])
+		print(Fore.BLUE + Style.BRIGHT + "ID: ",peliculas['Search'][x]['imdbID'])
 		print()
 
-	buscarID= input("Dime un id anteriormente dados para conocer mas información: ")
+	buscarID= input(Fore.GREEN + Style.BRIGHT+"Dime un id anteriormente dados para conocer mas información: ")
 	iddado=requests.get(raiz,
 							params={'i':buscarID}).json()
 	print()
-	print("¿Que quieres saber de esta película?")
-	print("1.Escritor y Director")
-	print("2.Género")
-	print("3.Premios")
-	print("4.Año de estreno")
-	print("5.Productora")
+	print(Fore.GREEN + Style.BRIGHT+"¿Que quieres saber de esta película?")
+	print(Fore.BLUE + Style.BRIGHT +"1.Escritor y Director")
+	print(Fore.BLUE + Style.BRIGHT +"2.Género")
+	print(Fore.BLUE + Style.BRIGHT +"3.Premios")
+	print(Fore.BLUE + Style.BRIGHT +"4.Año de estreno")
+	print(Fore.BLUE + Style.BRIGHT +"5.Productora")
 	print()
 
-	eleccion2=int(input("Escoge una de las opciones: "))
+	eleccion2=input(Fore.GREEN + Style.BRIGHT +"Escoge una de las opciones: ")
 
 	while eleccion2 != "salir":
-		if eleccion2 == 1:	
-			print("Escritor: ", iddado['Writer'])
-			print("Director: ",iddado['Director'])
-			break	
-		elif eleccion2 == 2:
-			print("Género: ",iddado['Genre'])
-		elif eleccion2 == 3:
-			print("Premios:",iddado['Awards'])	
-		elif eleccion2 == 4:
-			print("Fecha de estreno: ",iddado['Released'])
-		elif eleccion2 == 5:
-			print("Productora: ",iddado['Production'])	
-
+		if eleccion2 == "1":	
+			print(Fore.BLUE + Style.BRIGHT +"Escritor: ", iddado['Writer'])
+			print(Fore.BLUE + Style.BRIGHT +"Director: ",iddado['Director'])
+			print()
+		elif eleccion2 == "2":
+			print(Fore.BLUE + Style.BRIGHT +"Género: ",iddado['Genre'])
+			print()
+		elif eleccion2 == "3":
+			print(Fore.BLUE + Style.BRIGHT +"Premios:",iddado['Awards'])	
+			print()
+		elif eleccion2 == "4":
+			print(Fore.BLUE + Style.BRIGHT +"Fecha de estreno: ",iddado['Released'])
+			print()
+		elif eleccion2 == "5":
+			print(Fore.BLUE + Style.BRIGHT +"Productora: ",iddado['Production'])
+			print()	
+		else:
+			print(Fore.RED + Style.BRIGHT +"Opción incorrecta")
+			print()
+		eleccion2=input(Fore.GREEN + Style.BRIGHT +"Escoge una de las opciones. Teclee 'salir' para finalizar: ")
+			
 elif eleccion == 2:
 #Para hacer un request de series solamente.
-	buscar=input("Dime una serie: ")
+	buscar=input(Fore.GREEN+ Style.BRIGHT +"Dime una serie: ")
 	print()
 	series=requests.get(raiz,
 								params={'s':buscar,
 								'type':'series'}).json()
 	
-	for x in range (10):
-		print(series['Search'][x]['Title'])
-		print("ID: ",series['Search'][x]['imdbID'])
+	for x in range(5):
+		print(Fore.YELLOW + Style.BRIGHT +"Busquedas encontradas: ")
+		print(Fore.BLUE + Style.BRIGHT + series['Search'][x]['Title'])
+		print(Fore.BLUE + Style.BRIGHT +"ID: ",series['Search'][x]['imdbID'])
 		print()
 
-	buscarID= input("Dime un id anteriormente dados: ")
+	buscarID= input(Fore.GREEN + Style.BRIGHT+"Dime un id anteriormente dados: ")
 	iddado=requests.get(raiz,
 							params={'i':buscarID}).json()
 
 	print()
-	print("¿Que quieres saber de esta serie?")
-	print("1.Escritor y Director")
-	print("2.Género")
-	print("3.Premios")
-	print("4.Año de estreno")
+	print(Fore.GREEN + Style.BRIGHT+"¿Que quieres saber de esta serie?")
+	print(Fore.BLUE + Style.BRIGHT +"1.Escritor y Director")
+	print(Fore.BLUE + Style.BRIGHT +"2.Género")
+	print(Fore.BLUE + Style.BRIGHT +"3.Premios")
+	print(Fore.BLUE + Style.BRIGHT +"4.Año de estreno")
 	print()
 
-	eleccion2=int(input("Escoge una de las opciones: "))
-
-	if eleccion2 == 1:	
-		print("Escritor: ", iddado['Writer'])
-		print("Director: ",iddado['Director'])
-	elif eleccion2 == 2:
-		print("Género: ",iddado['Genre'])
-	elif eleccion2 == 3:
-		print("Premios:",iddado['Awards'])	
-	elif eleccion2 == 4:
-		print("Fecha de estreno: ",iddado['Released'])
-	else:
-		print("Opción incorrecta")
+	eleccion2=input(Fore.GREEN + Style.BRIGHT +"Escoge una de las opciones: ")
+	while eleccion2 != "salir":
+		if eleccion2 == "1":	
+			print(Fore.BLUE + Style.BRIGHT +"Escritor: ", iddado['Writer'])
+			print(Fore.BLUE + Style.BRIGHT +"Director: ",iddado['Director'])
+			print()
+		elif eleccion2 == "2":
+			print(Fore.BLUE + Style.BRIGHT +"Género: ",iddado['Genre'])
+			print()
+		elif eleccion2 == "3":
+			print(Fore.BLUE + Style.BRIGHT +"Premios:",iddado['Awards'])
+			print()
+		elif eleccion2 == "4":
+			print(Fore.BLUE + Style.BRIGHT +"Fecha de estreno: ",iddado['Released'])
+			print()
+		else:
+			print(Fore.RED + Style.BRIGHT +"Opción incorrecta")
+			print()
+		eleccion2=input(Fore.GREEN + Style.BRIGHT +"Escoge una de las opciones. Teclee 'salir' para finalizar: ")
 #Para hacer un request sin tipo, ya que hay más generos a parte de las opciones dadas.
 elif eleccion == 3:
-	buscar=input("Dime un juego o episodio: ")
+	buscar=input(Fore.GREEN+ Style.BRIGHT +"Dime un juego o episodio: ")
 	general=requests.get(raiz,	
 							params={'s':buscar}).json()
 	
-	for x in range (10):
-		print(general['Search'][x]['Title'])
-		print("ID: ",general['Search'][x]['imdbID'])
+	for x in range (5):
+		print(Fore.YELLOW + Style.BRIGHT +"Busquedas encontradas: ")
+		print(Fore.BLUE + Style.BRIGHT + general['Search'][x]['Title'])
+		print(Fore.BLUE + Style.BRIGHT +"ID: ",general['Search'][x]['imdbID'])
 		print()
 
-	buscarID= input("Dime un id anteriormente dados: ")
+	buscarID= input(Fore.GREEN + Style.BRIGHT+"Dime un id anteriormente dados: ")
 	iddado=requests.get(raiz,
 							params={'i':buscarID}).json()
 
 	print()
-	print("¿Que quieres saber de la búsqueda?")
-	print("1.Escritor y Director")
-	print("2.Género")
-	print("3.Premios")
-	print("4.Año de estreno")
+	print(Fore.GREEN + Style.BRIGHT+"¿Que quieres saber de la búsqueda?")
+	print(Fore.BLUE + Style.BRIGHT +"1.Escritor y Director")
+	print(Fore.BLUE + Style.BRIGHT +"2.Género")
+	print(Fore.BLUE + Style.BRIGHT +"3.Premios")
+	print(Fore.BLUE + Style.BRIGHT +"4.Año de estreno")
 	print()
-	eleccion2=int(input("Escoge una de las opciones: "))
 
-	if eleccion2 == 1:	
-		print("Escritor: ", iddado['Writer'])
-		print("Director: ",iddado['Director'])
-	elif eleccion2 == 2:
-		print("Género: ",iddado['Genre'])
-	elif eleccion2 == 3:
-		print("Premios:",iddado['Awards'])	
-	elif eleccion2 == 4:
-		print("Fecha de estreno: ",iddado['Released'])
-	elif eleccion2 == 5:
-		print("Productora: ",iddado['Production'])
-	else:
-		print("Opción incorrecta")
+	eleccion2=input(Fore.GREEN + Style.BRIGHT +"Escoge una de las opciones: ")
+	while eleccion2 != "salir":
+		if eleccion2 == "1":	
+			print(Fore.BLUE + Style.BRIGHT +"Escritor: ", iddado['Writer'])
+			print(Fore.BLUE + Style.BRIGHT +"Director: ",iddado['Director'])
+			print()
+		elif eleccion2 == "2":
+			print(Fore.BLUE + Style.BRIGHT +"Género: ",iddado['Genre'])
+			print()
+		elif eleccion2 == "3":
+			print(Fore.BLUE + Style.BRIGHT +"Premios:",iddado['Awards'])
+			print()	
+		elif eleccion2 == "4":
+			print(Fore.BLUE + Style.BRIGHT +"Fecha de estreno: ",iddado['Released'])
+			print()
+		elif eleccion2 == "5":
+			print(Fore.BLUE + Style.BRIGHT +"Productora: ",iddado['Production'])
+			print()
+		else:
+			print(Fore.RED + Style.BRIGHT +"Opción incorrecta")
+			print()
+		eleccion2=input(Fore.GREEN + Style.BRIGHT +"Escoge una de las opciones. Teclee 'salir' para finalizar: ")	
 
 elif eleccion == 4:
-	print("¡Hasta luego!")	
+	print(Fore.YELLOW + Style.BRIGHT +"¡Hasta luego!")	
 
 else:
-	print("Opción incorrecta")				
+	print(Fore.RED + Style.BRIGHT +"Opción incorrecta")				
 
 #PROBANDO EL FORMATO DEL JSON PELICULAS
 #with open("peliculas.json", "w") as archivo: 
